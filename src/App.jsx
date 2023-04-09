1c0fceecc1267e21b2359f40d078c91866c7ee40
Pamir Perdeci <pamirperdeci@gmail.com>
2023-04-08 23:52:29 -0400

    Button Component Renders Inside App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Buttons from './components/Buttons1'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <h1>How old are you?</h1>
+      <Buttons />
     </div>
   )
 }
---
+++ src/components/Buttons1.jsx
@@ -0,0 +1,8 @@
+
+
+
+export default function Buttons () {
+
+
+    return (<h1>Hello</h1>)
+}
